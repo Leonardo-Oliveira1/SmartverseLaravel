f01953fbcc026fe1635ce1a215ae91c2aff5a227
leonardo7k <oleonardo78@gmail.com>
2022-08-14 22:38:38 -0300

    fix: adicionando a metadescription

--- resources/views/creatingPage.blade.php
+++ resources/views/creatingPage.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/x-icon" href="{{ asset('css/img/moon_logo.svg') }}">
-        <meta name="description" content="@yield('metadescription')">
+        <meta name="description" content="{{ $post->metadescription }}">
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
         <title>{{ $post->title }} - Smartverse</title>
     </head>
